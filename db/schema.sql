94f92a735b28e269decf923118b2b5afab98ce87
Ashley Lin <linashleyy@gmail.com>
2025-06-09 11:38:10 +0800

    Create users, tasks tables

--- db/schema.sql
+++ db/schema.sql
@@ -1,1 +1,18 @@
+-- drop tasks first, bc users depends on it
+DROP TABLE IF EXISTS tasks;
+DROP TABLE IF EXISTS users;
 
+CREATE TABLE users(
+  id SERIAL PRIMARY KEY,
+  username TEXT NOT NULL,
+  password TEXT NOT NULL
+);
+
+CREATE TABLE tasks(
+  id SERIAL PRIMARY KEY,
+  title TEXT NOT NULL,
+  done BOOLEAN NOT NULL,
+  user_id INT NOT NULL
+    REFERENCES users(id) ON DELETE CASCADE,
+  UNIQUE(user_id, title)
+);
