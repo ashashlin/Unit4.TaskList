65203e40362b4cf0f17a0a9ec6adab0d226ba703
Ashley Lin <linashleyy@gmail.com>
2025-06-09 23:02:28 +0800

    Set up PUT /tasks/:id and DELETE /tasks/:id. All tests so far pass by testing manually.

--- api/routes/tasks.js
+++ api/routes/tasks.js
@@ -1,7 +1,12 @@
 import express from "express";
 import getUserFromToken from "#middleware/getUserFromToken";
 import requireUser from "#middleware/requireUser";
-import { createTask, getTasksByUserId } from "#db/queries/tasks";
+import {
+  createTask,
+  deleteTaskByIdAndUserId,
+  getTasksByUserId,
+  updateTaskByIdAndUserId,
+} from "#db/queries/tasks";
 import requireBody from "#middleware/requireBody";
 
 const tasksRouter = express.Router();
@@ -30,12 +35,55 @@ tasksRouter.post(
       const { title } = req.body;
       const done = req.body.done.toLowerCase() === "true";
 
-      await createTask(title, id, done);
-      res.sendStatus(201);
+      const task = await createTask(title, id, done);
+      res.status(201).send(task);
     } catch (error) {
       next(error);
     }
   }
 );
+
+// PUT /tasks
+tasksRouter.put(
+  "/:id",
+  requireBody(["title", "done"]),
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const { title } = req.body;
+      const done = req.body.done.toLowerCase() === "true";
+      const userId = req.user.id;
+
+      const task = await updateTaskByIdAndUserId(title, done, id, userId);
+      if (!task)
+        return res
+          .status(403)
+          .send("Error: you do not have access to this task.");
+
+      res.send(task);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+// DELETE /tasks
+tasksRouter.delete("/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const userId = req.user.id;
+
+    const deletedTask = await deleteTaskByIdAndUserId(id, userId);
+    if (!deletedTask)
+      return res
+        .status(403)
+        .send("Error: you do not have access to this task.");
+
+    // 204 No Content means “success with no response body”
+    res.sendStatus(204);
+  } catch (error) {
+    next(error);
+  }
+});
 
 export default tasksRouter;
--- db/queries/tasks.js
+++ db/queries/tasks.js
@@ -32,3 +32,36 @@ export async function getTasksByUserId(id) {
 
   return tasks;
 }
+
+export async function updateTaskByIdAndUserId(title, done, id, userId) {
+  const sql = `
+    UPDATE tasks
+    SET
+      title = $1,
+      done = $2
+    WHERE id = $3
+      AND user_id = $4
+    RETURNING *;
+  `;
+
+  const {
+    rows: [task],
+  } = await db.query(sql, [title, done, id, userId]);
+
+  return task;
+}
+
+export async function deleteTaskByIdAndUserId(id, userId) {
+  const sql = `
+    DELETE FROM tasks
+    WHERE id = $1
+      AND user_id = $2
+    RETURNING *;
+  `;
+
+  const {
+    rows: [deletedTask],
+  } = await db.query(sql, [id, userId]);
+
+  return deletedTask;
+}
